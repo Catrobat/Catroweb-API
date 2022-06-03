3ab472cf79900fa857821390dc888360a7e06f12
Daniel Metzner <daniel.metzner@outlook.com>
2022-06-03 23:06:46 +0200

    Fix duplicated use stmt in generator

--- Service/JmsSerializer.php
+++ Service/JmsSerializer.php
@@ -2,79 +2,77 @@
 
 namespace OpenAPI\Server\Service;
 
-use DateTime;
+use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Naming\CamelCaseNamingStrategy;
 use JMS\Serializer\Naming\SerializedNameAnnotationStrategy;
 use JMS\Serializer\Serializer;
-use JMS\Serializer\SerializerBuilder;
-use JMS\Serializer\SerializerBuilder;
 use JMS\Serializer\Visitor\Factory\XmlDeserializationVisitorFactory;
+use DateTime;
 use RuntimeException;
 
 class JmsSerializer implements SerializerInterface
 {
-  protected Serializer $serializer;
+    protected Serializer $serializer;
 
-  public function __construct()
-  {
-    $namingStrategy = new SerializedNameAnnotationStrategy(new CamelCaseNamingStrategy());
-    $this->serializer = SerializerBuilder::create()
-      ->setDeserializationVisitor('json', new StrictJsonDeserializationVisitorFactory())
-      ->setDeserializationVisitor('xml', new XmlDeserializationVisitorFactory())
-      ->setPropertyNamingStrategy($namingStrategy)
-      ->build()
-    ;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function serialize($data, string $format): string
-  {
-    return SerializerBuilder::create()->build()->serialize($data, $this->convertFormat($format));
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function deserialize($data, $type, $format)
-  {
-    if ('string' == $format) {
-      return $this->deserializeString($data, $type);
+    public function __construct()
+    {
+        $namingStrategy = new SerializedNameAnnotationStrategy(new CamelCaseNamingStrategy());
+        $this->serializer = SerializerBuilder::create()
+            ->setDeserializationVisitor('json', new StrictJsonDeserializationVisitorFactory())
+            ->setDeserializationVisitor('xml', new XmlDeserializationVisitorFactory())
+            ->setPropertyNamingStrategy($namingStrategy)
+            ->build();
     }
 
-    // If we end up here, let JMS serializer handle the deserialization
-    return $this->serializer->deserialize($data, $type, $this->convertFormat($format));
-  }
+    /**
+     * @inheritdoc
+     */
+    public function serialize($data, string $format): string
+    {
+        return SerializerBuilder::create()->build()->serialize($data, $this->convertFormat($format));
+    }
 
-  private function convertFormat($format): ?string
-  {
-    switch ($format) {
+    /**
+     * @inheritdoc
+     */
+    public function deserialize($data, $type, $format)
+    {
+        if ($format == 'string') {
+            return $this->deserializeString($data, $type);
+        }
+
+        // If we end up here, let JMS serializer handle the deserialization
+        return $this->serializer->deserialize($data, $type, $this->convertFormat($format));
+    }
+
+    private function convertFormat($format): ?string
+    {
+        switch ($format) {
             case 'application/json':
                 return 'json';
             case 'application/xml':
                 return 'xml';
         }
 
-    return null;
-  }
-
-  private function deserializeString($data, $type)
-  {
-    // Figure out if we have an array format
-    if (1 === preg_match('/array<(csv|ssv|tsv|pipes),(int|string)>/i', $type, $matches)) {
-      return $this->deserializeArrayString($matches[1], $matches[2], $data);
+        return null;
     }
 
-    switch ($type) {
+    private function deserializeString($data, $type)
+    {
+        // Figure out if we have an array format
+        if (1 === preg_match('/array<(csv|ssv|tsv|pipes),(int|string)>/i', $type, $matches)) {
+            return $this->deserializeArrayString($matches[1], $matches[2], $data);
+        }
+
+        switch ($type) {
             case 'int':
             case 'integer':
                 if (is_int($data)) {
-                  return $data;
+                    return $data;
                 }
 
                 if (is_numeric($data)) {
-                  return $data + 0;
+                    return $data + 0;
                 }
 
                 break;
@@ -82,12 +80,12 @@ class JmsSerializer implements SerializerInterface
                 break;
             case 'boolean':
             case 'bool':
-                if ('true' === strtolower($data)) {
-                  return true;
+                if (strtolower($data) === 'true') {
+                    return true;
                 }
 
-                if ('false' === strtolower($data)) {
-                  return false;
+                if (strtolower($data) === 'false') {
+                    return false;
                 }
 
                 break;
@@ -95,21 +93,21 @@ class JmsSerializer implements SerializerInterface
             case '\DateTime':
                 return new DateTime($data);
             default:
-                throw new RuntimeException(sprintf('Type %s is unsupported', $type));
+                throw new RuntimeException(sprintf("Type %s is unsupported", $type));
         }
 
-    // If we end up here, just return data
-    return $data;
-  }
-
-  private function deserializeArrayString(string $format, string $type, ?array $data): array
-  {
-    if (null === $data) {
-      return [];
+        // If we end up here, just return data
+        return $data;
     }
 
-    // Parse the string using the correct separator
-    switch ($format) {
+    private function deserializeArrayString(string $format, string $type, ?array $data): array
+    {
+        if ($data === null) {
+            return [];
+        }
+
+        // Parse the string using the correct separator
+        switch ($format) {
             case 'csv':
                 $data = explode(',', $data);
                 break;
@@ -122,15 +120,15 @@ class JmsSerializer implements SerializerInterface
             case 'pipes':
                 $data = explode('|', $data);
                 break;
-            default:
+            default;
                 $data = [];
         }
 
-    // Deserialize each of the array elements
-    foreach ($data as $key => $item) {
-      $data[$key] = $this->deserializeString($item, $type);
-    }
+        // Deserialize each of the array elements
+        foreach ($data as $key => $item) {
+            $data[$key] = $this->deserializeString($item, $type);
+        }
 
-    return $data;
-  }
+        return $data;
+    }
 }
--- Tests/AppKernel.php
+++ Tests/AppKernel.php
@@ -3,25 +3,27 @@
 namespace OpenAPI\Server\Tests;
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Component\Config\Loader\LoaderInterface;
 use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
 
 class AppKernel extends Kernel
 {
-  public function registerBundles(): iterable
-  {
-    return [
-      new FrameworkBundle(),
-    ];
-  }
+    public function registerBundles(): iterable
+    {
+        $bundles = array(
+            new FrameworkBundle()
+        );
 
-  /**
-   * @throws \Exception
-   *
-   * @return mixed
-   */
-  public function registerContainerConfiguration(LoaderInterface $loader)
-  {
-    $loader->load(__DIR__.'/test_config.yml');
-  }
+        return $bundles;
+    }
+
+    /**
+     * @param LoaderInterface $loader
+     * @return mixed
+     * @throws \Exception
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/test_config.yml');
+    }
 }
