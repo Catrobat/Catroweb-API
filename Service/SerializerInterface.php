daf7e879663210918b8aa5be20c6bd618a2c3a7e
dmetzner <dmetzner@users.noreply.github.com>
2023-10-23 13:29:20 +0000

    Automated Code Generation

--- Service/SerializerInterface.php
+++ Service/SerializerInterface.php
@@ -14,8 +14,6 @@ interface SerializerInterface
   /**
    * Deserializes the given data to the specified type.
    *
-   * @param mixed $data
-   *
    * @return object|array|scalar
    */
   public function deserialize($data, string $type, string $format);
